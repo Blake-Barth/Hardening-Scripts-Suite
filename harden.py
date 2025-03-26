709fa919bcca94bbf3102a8914e8af05276ed302
Blake-Barth <blake.barth.cybersec@gmail.com>
2025-03-26 14:59:21 -0400

    Update harden.py

--- harden.py
+++ harden.py
@@ -5,8 +5,6 @@ def check_admin():
     if os.geteuid() != 0:
         print("You need to run this script as root (sudo).")
         sys.exit(1)
-    else:
-        print("You are running as root.")
 
 if __name__ == "main":
   check_admin()
